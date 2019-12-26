2b6489ee010646dbc176e4a6bed654966ef3c713
sunakshi96 <sunakshitejwani@gmail.com>
2019-12-26 15:38:54 +0530

    fetch content  from api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      monsters:[
-        {
-          name:"Frankestein",
-          id:"asc1"
-        },
-        {
-          name:"Dracula",
-          id:"asc2"
-        },
-        {
-          name:"Zombie",
-          id:"asc3"
-        }
-      ]
+      monsters:[]
     }
   }
-  
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
+
   render(){
     return (
       <div className="App">
