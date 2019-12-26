6ea758e4cac5ddd44615297a23661e3b436d158f
sunakshi96 <sunakshitejwani@gmail.com>
2019-12-26 17:59:34 +0530

    add image,style and email to each monster card

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+import "./card.style.css";
 
-export const Card = (props) => (
-    <div>
-        <h1>{props.monster.name}</h1>
-    </div>
+export const Card = props => (
+  <div className="card-container">
+    <img
+      alt="monster"
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+    ></img>
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
 );
