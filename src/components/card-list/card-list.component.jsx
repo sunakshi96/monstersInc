afc453d6a8a327a5a6191f139373fdcc0b9915de
sunakshi96 <sunakshitejwani@gmail.com>
2019-12-26 17:29:37 +0530

    pass props in CardList component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,11 @@ import './card-list.style.css';
 
 export const CardList = (props) => {
     return(
-        <div className="card-list">{props.children}</div>
+        <div className="card-list">
+            {props.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </div>
     )
     
 }
